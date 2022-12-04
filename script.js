2ea65e89bce40f59265eadc5765cd5389f632442
Caique Costa <ccostafrias@gmail.com>
2022-12-04 20:44:57 -0300

    Pós tela + popup + efeitinhos
    Provavelmente o último update da v1.0. O site agora é 100% funcional, impede que os campos fiquem vazios e permite voltar e avançar.

--- script.js
+++ script.js
@@ -9,15 +9,18 @@ const progressBar = document.getElementsByClassName("progressInside")
 const display = document.getElementById("display")
 const caption = document.getElementById("caption")
 const triesTxt = document.getElementById("triesTxt").children[0]
+const lwScreen = document.getElementById("lw-screen")
+const popup = document.getElementById("error-popup")
 let displayX = []
 let displayY = []
 let displayZ = []
 let tries = 3
 let elements = 3
-let total = 15
+let goal = 10
 let hits = 0
+let score = 0
 let misses = 0
-let displayFontSize = ""
+let displayFontSize = 96
 let arrayKana = [
     [["あ", "い", "う", "え", "お"],
     ["か", "き", "く", "け", "こ"],
@@ -73,6 +76,119 @@ function load(){
     elements = window.localStorage.getItem('elements')
     document.getElementById("elements").value = elements
 }
+function start(){
+    setlist()
+    tries = document.getElementById("tries").value
+    elements = document.getElementById("elements").value
+    if (tries > 0 && elements > 0){
+        if (countH.length !== 0 || countK.length !== 0){
+            popup.classList.remove('active')
+            interface.style.display = "none"
+            lwScreen.style.display = 'none'
+            quiz.style.display = "block"
+            quiz.style.pointerEvents = "auto"
+            quiz.style.opacity = 1
+            hits = 0
+            score = 0
+            misses = 0
+            setmisses()
+            if (elements >= 5){
+                displayFontSize = section[0].offsetWidth * 8/10 / elements
+            }else{
+                displayFontSize = 96
+            }
+            caption.children[0].innerHTML = "Insira em <em>romanji</em> o que está escrito em <em>nihongo</em>"
+            caption.style.backgroundColor = "var(--main)"
+            inputAns.style.width = displayFontSize*elements*(105/100) + "px"
+            while (inputAns.hasChildNodes()) {
+                inputAns.removeChild(inputAns.firstChild);
+            }
+            for (let i = 0; i < elements; i++){
+                var inputElement = document.createElement('input')
+                inputElement.setAttribute('type', 'text')
+                inputElement.setAttribute('class', 'answer')
+                inputElement.setAttribute('id', 'answer' + i)
+                inputElement.style.width = (displayFontSize * 7/10) + "px"
+                inputElement.addEventListener('keyup', function(e){
+                    if (e.which == 13){
+                        answersLength = []
+                        for (let i = 0; i < inputAnswers.length; i++){
+                            if (String(inputAnswers[i].value).toLocaleLowerCase() == arrayRomanji[displayY[i]][displayZ[i]]){
+                                answersLength.push(i)
+                            }else if (inputAnswers[i].value == ""){
+                                answersLength.push("")
+                            }
+                        }
+                        if (answersLength.indexOf("") == -1){
+                            if (answersLength.length == elements){
+                                caption.children[0].innerHTML = "<strong>Acertou! </strong>"
+                                caption.style.backgroundColor = "var(--green)"
+                                score ++
+                                hits ++
+                                if (score == goal){
+                                    fadeout(quiz, "ganhou!", "green")
+                                }
+                                progressBar[0].style.width = (score/goal)*100 + "%"
+                            }else{
+                                caption.children[0].innerHTML = "<strong>Errou! </strong>"
+                                caption.style.backgroundColor = "var(--red)"
+                                score > 0? score -- : score = 0
+                                misses ++
+                                hits ++
+                                setmisses()
+                                if (tries == misses){
+                                    fadeout(quiz, "perdeu...", "red")
+                                }
+                                progressBar[0].style.width = (score/goal)*100 + "%"
+                            }
+                            for (let i = 0; i < elements; i++) {
+                                caption.children[0].innerHTML += arrayKana[displayX[i]][displayY[i]][displayZ[i]]
+                            }
+                            caption.children[0].innerHTML += " = "
+                            for (let i = 0; i < elements; i++) {
+                                caption.children[0].innerHTML += arrayRomanji[displayY[i]][displayZ[i]]
+                            }
+                            input(elements);
+                        }
+                    }
+                })
+                inputAns.appendChild(inputElement)
+                if (i == 0){
+                    inputElement.focus()
+                }
+            }
+            input(elements)
+        }else{
+            popup.classList.toggle('active')
+            popup.children[0].innerHTML = "Selecione pelo menos uma fileira de <em>kanas</em>!"  
+        }
+    }else{
+        popup.classList.toggle('active')
+        popup.children[0].innerHTML = "Preencha os campos com números válidos!"
+    }
+}
+function back(){
+    interface.style.display = "block"
+    lwScreen.style.display = "none"
+    quiz.style.display = "none"
+}
+function fadeout(element, lw, txtColor){
+    quiz.style.pointerEvents = "none"
+    var op = 1
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer)
+            element.style.display = 'none'
+            lwScreen.style.display = 'block'
+            lwScreen.children[0].innerHTML = "Você " + lw
+            lwScreen.children[0].style.color = "var(--" + txtColor + ")"
+            lwScreen.children[1].children[0].innerHTML = "Tentativas: " + hits
+            lwScreen.children[1].children[1].innerHTML = "Precisão: " + Math.round(((hits - misses)/hits) * 100) + "%"
+        }
+        element.style.opacity = op
+        op -= op * 0.1
+    }, 50);
+}
 function saveLocalStorage(key){
     window.localStorage.setItem(key, document.getElementById(key).value)
 }
@@ -92,62 +208,14 @@ function setlist(){
     }
     window.localStorage.setItem('listK', JSON.stringify(countK))
 }
-function start(){
-    setlist()
-    if (countH.length !== 0 || countK.length !== 0){
-        interface.style.display = "none"
-        quiz.style.display = "block"
-        tries = document.getElementById("tries").value
-        elements = document.getElementById("elements").value
-        displayFontSize = (section[0].offsetWidth * ((getBaseLog(2, elements) + 3)/10)) / elements
-        inputAns.style.width = displayFontSize*elements*(105/100) + "px"
-        for (let i = 0; i < elements; i++){
-            var inputElement = document.createElement('input')
-            inputElement.setAttribute('type', 'text')
-            inputElement.setAttribute('class', 'answer')
-            inputElement.setAttribute('id', 'answer' + i)
-            inputElement.style.width = (displayFontSize * 7/10) + "px"
-            inputElement.addEventListener('keyup', function(e){
-                if (e.which == 13){
-                    answersLength = []
-                    for (let i = 0; i < inputAnswers.length; i++){
-                        if (String(inputAnswers[i].value).toLocaleLowerCase() == arrayRomanji[displayY[i]][displayZ[i]]){
-                            answersLength.push(i)
-                        }else if (inputAnswers[i].value == ""){
-                            answersLength.push("")
-                        }
-                    }
-                    if (answersLength.indexOf("") == -1){
-                        if (answersLength.length == elements){
-                            caption.children[0].innerHTML = "<strong>Acertou! </strong>"
-                            caption.style.backgroundColor = "var(--green)"
-                            hits ++
-                            progressBar[0].style.width = (hits/total)*100 + "%"
-                        }else{
-                            caption.children[0].innerHTML = "<strong>Errou! </strong>"
-                            caption.style.backgroundColor = "var(--red)"
-                            hits --
-                            misses ++
-                            triesTxt.innerHTML += '\uf00d'
-                            progressBar[0].style.width = (hits/total)*100 + "%"
-                        }
-                        for (let i = 0; i < elements; i++) {
-                            caption.children[0].innerHTML += arrayKana[displayX[i]][displayY[i]][displayZ[i]]
-                        }
-                        caption.children[0].innerHTML += " = "
-                        for (let i = 0; i < elements; i++) {
-                            caption.children[0].innerHTML += arrayRomanji[displayY[i]][displayZ[i]]
-                        }
-                        input(elements);
-                    }
-                }
-            })
-            inputAns.appendChild(inputElement)
-            if (i == 0){
-                inputElement.focus()
-            }
-        }
-        input(elements)
+function setmisses(){
+    triesTxt.children[0].innerHTML = ""
+    triesTxt.children[1].innerHTML = ""
+    for (let i = 0; i < misses; i++) {
+        triesTxt.children[0].innerHTML += '\uf00d'
+    }
+    for (let i = 0; i < tries - misses; i++) {
+        triesTxt.children[1].innerHTML += '\uf00d'
     }
 }
 function allBoxes(list){
